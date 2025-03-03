1b53a9bc2fdf5d0dfc3a01ba6b0aa145de26f5ad
ayanpandey27 <ayanroyal1174@gmail.com>
2025-03-03 21:48:14 +0530

    UC2

--- src/main/java/com/example/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -1,4 +1,5 @@
 package com.example.GreetingApp.controller;
+import com.example.GreetingApp.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/message")
     public String getMessage() {
-        return "Hello, Welcome to the Greeting App!";
+        return greetingService.getGreetingMessage();
     }
 }
